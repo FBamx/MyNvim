7955d72c73008bdd56b87c4dc463812e34c9efa3
root <root@DESKTOP-0T01DAB>
2023-05-09 02:31:22 +0800

    add

--- init.lua
+++ init.lua
@@ -1,19 +1,19 @@
 local echo = function(str)
-  vim.cmd("redraw")
-  vim.api.nvim_echo({ { str, "Bold" } }, true, {})
+	vim.cmd("redraw")
+	vim.api.nvim_echo({ { str, "Bold" } }, true, {})
 end
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  echo("  Installing lazy.nvim & plugins ...")
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	echo("  Installing lazy.nvim & plugins ...")
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,6 +21,7 @@ require("util")
 require("config.keymaps")
 require("config.autocmds")
 require("config.options")
+require("core")
 require("lazy").setup("plugins")
 
 -- colorscheme
@@ -31,3 +32,5 @@ vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
 
 local config = require("core.default_config").ui
 vim.opt.statusline = "%!v:lua.require('ui.statusline." .. config.statusline.theme .. "').run()"
+
+dofile(vim.g.base46_cache .. "defaults")
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -388,4 +388,11 @@ return {
 			})
 		end,
 	},
+	{
+		"NvChad/base46",
+		branch = "v2.0",
+		build = function()
+			require("base46").load_all_highlights()
+		end,
+	},
 }
